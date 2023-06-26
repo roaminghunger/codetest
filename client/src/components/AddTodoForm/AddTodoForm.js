d76b656e9280ac40f3b6dcf407f1f2f529fe87cc
NatalieCC <53953709+NatalieCC@users.noreply.github.com>
2023-06-26 01:13:17 -0700

    Created AddTodoForm component for adding new todos and TodoList for displaying todos

---
+++ client/src/components/AddTodoForm/AddTodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useCreateTodo } from '../../hooks/useCreateTodo';
+
+export default function AddTodoForm() {
+  const [value, setValue] = useState('');
+  const { createTodo } = useCreateTodo();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (value.trim()) {
+      createTodo(value);
+      setValue('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="AddTodoForm">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="What needs to be done?"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+}
---
+++ client/src/components/TodoList/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodoItem from '../TodoItem/TodoItem';
+import PropTypes from 'prop-types';
+
+export default function TodoList({ todos }) {
+  return (
+    <ul className="TodoList">
+      {todos.map((todo) => (
+        <TodoItem key={todo._id} todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      data: PropTypes.string.isRequired,
+      done: PropTypes.bool.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
