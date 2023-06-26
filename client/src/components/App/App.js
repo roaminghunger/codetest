5391727e675598b08f9af1a801b50f50ea82ad23
NatalieCC <53953709+NatalieCC@users.noreply.github.com>
2023-06-26 01:10:02 -0700

    Implement TodoContainer component and to be rendered in App.js

---
+++ client/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoContainer from '../TodoContainer/TodoContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TodoContainer/TodoContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AddTodoForm from '../AddTodoForm/AddTodoForm';
+import TodoList from '../TodoList/TodoList';
+import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
+import { useFetchTodos } from '../../hooks/useFetchTodos';
+
+export default function TodoContainer() {
+  const { todos, loading, error, clearApiError } = useFetchTodos();
+
+  if (loading) {
+    return <LoadingIndicator />; // Display a loading indicator if fetching is in process
+  }
+
+  if (error) {
+    return (
+      <div className="error">
+        Error: {error}
+        <button onClick={clearApiError}>Close</button>{' '}
+      </div>
+    );
+  }
+
+  return (
+    <div className="TodoContainer">
+      <AddTodoForm />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
